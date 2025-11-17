8b47bf26ec32bb48666b5825840be09f961db173
Balbino <balbino@balbino.balbino>
2025-11-17 13:50:08 +0100

    registro

--- register.php
+++ register.php
@@ -9,15 +9,15 @@
     ?>
 </head>
 <body>
-    <div class="registro-pc">
+<div class="registro-pc">
   <div class="frame-11">
-      <img class="logo" src="img/kairos.png" />
+    <img class="logo" src="/img/kairos.png" />
   </div>
   <div class="registro">
     <div class="frame-9">
       <div class="frame-20">
         <div class="a-n-no-tienes-cuenta-registate">
-          ¿Aún no tienes cuenta? Registrate
+          ¿Aún no tienes cuenta? Registate
         </div>
       </div>
       <div class="frame-12">
@@ -25,15 +25,15 @@
           <div class="nombre">Nombre</div>
           <div class="frame-17"></div>
         </div>
-        <div class="frame-172">
+        <div class="frame-25">
           <div class="apellidos">Apellidos</div>
           <div class="frame-17"></div>
         </div>
-        <div class="frame-18">
+        <div class="frame-26">
           <div class="email">Email</div>
           <div class="frame-17"></div>
         </div>
-        <div class="frame-19">
+        <div class="frame-27">
           <div class="contrase-a">Contraseña</div>
           <div class="frame-17"></div>
         </div>
@@ -49,5 +49,6 @@
     </div>
   </div>
 </div>
+
 </body>
 </html>
