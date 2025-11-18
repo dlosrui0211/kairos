8cbbdda6a63947bc8e4781dd84a85bb09b0a854c
Juan Carlos <jtorber955@iesmarquesdecomares.org>
2025-11-18 11:24:53 +0100

    recibos v4

--- recibos.php
+++ recibos.php
@@ -1,30 +1,19 @@
 <?php include("includes/a_config.php");?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Recibos - Kairos</title>
+    <title>Recibos</title>
     <?php include_once 'includes/head-tag-contents.php' ?>
 </head>
 
-
-
-
-
 <body>
-
 <?php include("includes/navigation.php");?>
 <main>
-
-
-    <h1>RECIBOS</h1>
-
     <div class="lista-contenedor">
-
-
         <p class="titulo">Recibos</p>
-
         <div class="recibo-card">
             <div class="recibo-left">
                 <img class="recibo-icono" src="assets/img/recibo.png" />
@@ -33,13 +22,11 @@
                     <p>Solicitar devolución</p>
                 </div>
             </div>
-
             <div class="recibo-right">
                 <span>30 días</span>
                 <img class="icono-basura" src="assets/img/papelera.png" />
             </div>
         </div>
-
         <div class="recibo-card">
             <div class="recibo-left">
                 <img class="recibo-icono" src="assets/img/recibo.png" />
@@ -48,13 +35,11 @@
                     <p>Solicitar devolución</p>
                 </div>
             </div>
-
             <div class="recibo-right">
                 <span>30 días</span>
                 <img class="icono-basura" src="assets/img/papelera.png" />
             </div>
         </div>
-
         <div class="recibo-card">
             <div class="recibo-left">
                 <img class="recibo-icono" src="assets/img/recibo.png" />
@@ -70,14 +55,8 @@
             </div>
         </div>
     </div>
-
-
 </main>
-
 <?php include("includes/footer.php");?>
-
-
-
 </body>
 
 </html>
