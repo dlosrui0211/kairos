74b2f958232ebefa9293dc53ec3e34182a689ec6
Daniel <dlosrui0211@iesmarquesdecomares.org>
2025-11-20 14:57:23 +0100

    Nuevos cambios

---
+++ devoluciones.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Devoluciones - kairos</title>
+    <?php  include_once 'includes/head-tag-contents.php'?>
+</head>
+<body>
+    <?php include_once 'includes/navigation.php' ?>
+    <div class="devoluciones-pc">
+        <div class="lista-devoluciones">
+            <div class="frame-9">
+            <div class="frame-25">
+                <div class="frame-26">
+                    <div class="frame-27">
+                        <div class="devoluci-n">DEVOLUCIÓN</div>
+                    </div>
+                    <div class="frame-28">
+                        <div class="subtotal-x-productos">Subtotal: (X productos)</div>
+                        <div class="frame-29">
+                            <div class="_99">99€</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="frame-29">
+                    <div class="coste-de-recogida-a-domicilio">
+                        Coste de recogida a domicilio
+                    </div>
+                    <div class="frame-29">
+                        <div class="_2-99">2,99€</div>
+                    </div>
+                </div>
+                <div class="frame-30">
+                    <div
+                    class="el-importe-de-la-devoluci-n-se-reembolsar-en-el-mismo-m-todo-de-pago-en-el-que-realiz-la-compra-en-un-plazo-m-ximo-de-10-d-as-h-biles"
+                    >
+                        El importe de la devolución se reembolsará en el mismo método de
+                        pago en el que realizó la compra en un plazo máximo de 10 días
+                        hábiles.
+                    </div>
+                </div>
+                <div class="frame-31">
+                    <div class="div">---------------------------------------------------------------------------------------------------------------------------------------------------------</div>
+                </div>
+                <div class="frame-33">
+                    <div class="total-de-la-devoluci-n">Total de la devolución:</div>
+                    <div class="frame-29">
+                        <div class="_992">99€</div>
+                    </div>
+                </div>
+
+                <div class="frame-7">
+                    <div class="confirmar-devoluci-n">Confirmar devolución</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php  include_once 'includes/footer.php'?>
+</body>
+</html>
+
+
+
+
+
