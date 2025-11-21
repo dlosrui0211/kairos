08ba9a800d1d9b350b45c0df70d49fb88ef2c43e
Daniel <dlosrui0211@iesmarquesdecomares.org>
2025-11-21 02:06:27 +0100

    Parte - Daniel

---
+++ wishlist.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wishlist - Kairos</title>
+    <?php include_once 'includes/head-tag-contents.php'?>
+</head>
+<body>
+    <?php include_once 'includes/navigation.php' ?>
+    <?php include_once 'includes/carrito.php' ?>
+    <div class="wishlist-pc">
+    <div class="lista-de-deseados">
+        <div class="lista-de-deseos-pc">
+        <div class="tarjeta">
+            <div class="publicacion">
+            <img class="anuncio-de-juego" src="/assets/img/imagen-placeholder.png" />
+            </div>
+            <div class="texto">
+            <div class="day-z">DayZ</div>
+            <div class="reseñas">
+                Reseñas en español de españa: MUY POSITIVOS
+            </div>
+            <div class="fecha-de-lanzamiento-13-12-2018">
+                Fecha de Lanzamiento: 13/12/2018
+            </div>
+            </div>
+            <div class="precio-carrito">
+            <div class="frame-precio">
+                <div class="_47-99">47,99€</div>
+            </div>
+            <img class="frame-linea" src="frame-linea0.svg" />
+            <div class="frame-a-adir-al-carrito">
+                <div class="a-adir-al-carro">Añadir al carro</div>
+            </div>
+            </div>
+        </div>
+        <div class="tarjeta2">
+            <div class="publicacion">
+            <img class="anuncio-de-juego" src="/assets/img/imagen-placeholder.png" />
+            </div>
+            <div class="texto">
+            <div class="day-z">DayZ</div>
+            <div class="reseñas">
+                Reseñas en español de españa: MUY POSITIVOS
+            </div>
+            <div class="fecha-de-lanzamiento-13-12-2018">
+                Fecha de Lanzamiento: 13/12/2018
+            </div>
+            </div>
+            <div class="precio-carrito">
+            <div class="frame-precio">
+                <div class="_47-99">47,99€</div>
+            </div>
+            <img class="frame-linea2" src="frame-linea1.svg" />
+            <div class="frame-a-adir-al-carrito">
+                <div class="a-adir-al-carro">Añadir al carro</div>
+            </div>
+            </div>
+        </div>
+        <div class="tarjeta2">
+            <div class="publicacion">
+            <img class="anuncio-de-juego" src="/assets/img/imagen-placeholder.png" />
+            </div>
+            <div class="texto">
+            <div class="day-z">DayZ</div>
+            <div class="reseñas">
+                Reseñas en español de españa: MUY POSITIVOS
+            </div>
+            <div class="fecha-de-lanzamiento-13-12-2018">
+                Fecha de Lanzamiento: 13/12/2018
+            </div>
+            </div>
+            <div class="precio-carrito">
+            <div class="frame-precio">
+                <div class="_47-99">47,99€</div>
+            </div>
+            <img class="frame-linea3" src="frame-linea2.svg" />
+            <div class="frame-a-adir-al-carrito">
+                <div class="a-adir-al-carro">Añadir al carro</div>
+            </div>
+            </div>
+        </div>
+        <div class="tarjeta2">
+            <div class="publicacion">
+            <img class="anuncio-de-juego" src="/assets/img/imagen-placeholder.png" />
+            </div>
+            <div class="texto">
+            <div class="day-z">DayZ</div>
+            <div class="reseñas">
+                Reseñas en español de españa: MUY POSITIVOS
+            </div>
+            <div class="fecha-de-lanzamiento-13-12-2018">
+                Fecha de Lanzamiento: 13/12/2018
+            </div>
+            </div>
+            <div class="precio-carrito">
+            <div class="frame-precio">
+                <div class="_47-99">47,99€</div>
+            </div>
+            <img class="frame-linea4" src="frame-linea3.svg" />
+            <div class="frame-a-adir-al-carrito">
+                <div class="a-adir-al-carro">Añadir al carro</div>
+            </div>
+            </div>
+        </div>
+        <div class="boton">
+            <div class="frame-8">
+            <div class="eliminar-todo">Eliminar Todo</div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+    <?php include_once 'includes/footer.php'?>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
